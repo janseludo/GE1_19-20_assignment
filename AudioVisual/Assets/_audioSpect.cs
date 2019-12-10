0ac75fd9d7f14233d7d74fb751eb9d5b290c7b98
janseludo <jan.seludo@mydit.ie>
2019-12-10 22:28:51 +0000

    Project

---
+++ AudioVisual/Assets/_audioSpect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _audioSpect : MonoBehaviour
+{
+    private void Update()
+    {
+        // get the data
+        AudioListener.GetSpectrumData(m_audioSpectrum, 0, FFTWindow.Hamming);
+        if (m_audioSpectrum != null && m_audioSpectrum.Length > 0)
+        {
+            spectrumValue = m_audioSpectrum[0] * 100;
+        }
+    }
+
+    private void Start()
+    {
+        //initialize buffer
+        m_audioSpectrum = new float[128];
+    }
+    // This value served to AudioSyncer for beat extraction
+    public static float spectrumValue
+    {
+        get; private set;
+    }
+    // Unity fills this up for us
+    private float[] m_audioSpectrum;
+
+}
---
+++ AudioVisual/Assets/_audioSync.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class _audioSync : MonoBehaviour
+{
+    public float bias;                      //what spectrum value to trigger a beat
+    public float timeStep;                  //determins the minim interval between each beat
+    public float timeToBeat;                //how much time between the visualization completes
+    public float restSmoothTime;            //how fast the object goes to rest after a beat
+
+    //if the value went above or below the value during the current frame, thus triggereing a beat
+    private float m_previousAudioValue;
+    private float m_audioValue;
+
+    //ot keep strack of the time step interval
+    private float m_timer;
+
+    //bool to determin if the sync object in in a beat state
+    protected bool m_isBeat;
+
+    // Inherit this to cause some behavior on each beat
+    public virtual void OnBeat()
+    {
+        Debug.Log("beat");
+        m_timer = 0;
+        m_isBeat = true;
+    }
+
+    //subclasses to inject thier own update without completely overwriting the base class
+    public virtual void OnUpdate()
+    {
+        // update audio value
+        m_previousAudioValue = m_audioValue;
+        m_audioValue = _audioSpect.spectrumValue;
+
+        // if audio value went below the bias during this frame
+        if (m_previousAudioValue > bias && m_audioValue <= bias)
+        {
+            // if minimum beat interval is reached
+            if (m_timer > timeStep)
+                OnBeat();
+        }
+
+        // if audio value went above the bias during this frame
+        if (m_previousAudioValue <= bias &&  m_audioValue > bias)
+        {
+            // if minimum beat interval is reached
+            if (m_timer > timeStep)
+                OnBeat();
+        }
+
+        m_timer += Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        OnUpdate();
+    }
+
+    
+}
