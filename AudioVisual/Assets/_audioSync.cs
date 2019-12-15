bb0a2f6d27842777fd61cf3bcecc0aa6bd1000b8
janseludo <jan.seludo@mydit.ie>
2019-12-15 15:57:23 +0000

    GE1 Assignment

--- AudioVisual/Assets/_audioSync.cs
+++ AudioVisual/Assets/_audioSync.cs
@@ -5,30 +5,23 @@ using UnityEngine;
 
 public class _audioSync : MonoBehaviour
 {
-    //what spectrum value to trigger a beat
-    public float bias;
-    //determins the minim interval between each beat
-    public float timeStep;
-    //how much time between the visualization completes
-    public float timeToBeat;
-    //how fast the object goes to rest after a beat
-    public float restSmoothTime;
+    public float bias;          //what spectrum value to trigger a beat
+    public float timeStep;      //determins the minim interval between each beat
+    public float timeToBeat;    //how much time between the visualization completes
+    public float restSmoothTime;//how fast the object goes to rest after a beat
 
     //if the value went above or below the value 
     //during the current frame, thus triggereing a beat
     private float m_previousAudioValue;
     private float m_audioValue;
 
-    //ot keep strack of the time step interval
-    private float m_timer;
-
-    //bool to determin if the sync object in in a beat state
-    protected bool m_isBeat;
+    private float m_timer;      //to keep strack of the time step interval
+    protected bool m_isBeat;    //bool to determin if the sync object in in a beat state
 
     // Inherit this to cause some behavior on each beat
     public virtual void OnBeat()
     {
-        Debug.Log("beat");
+        //Debug.Log("beat");
         m_timer = 0;
         m_isBeat = true;
     }
@@ -58,7 +51,7 @@ public class _audioSync : MonoBehaviour
         }
         m_timer += Time.deltaTime;
     }
-    private void Update()
+    public void Update()
     {
         OnUpdate();
     }
---
+++ AudioVisual/Assets/phyllotaxis.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class phyllotaxis : MonoBehaviour
+{
+    public float _degree, _scale;
+    public int _numberStart;
+    public int _stepSize;
+
+    public int _maxIntegration;
+
+    //Learping
+    public bool _useLearping;
+    public float _intervalLerp;
+    private bool _isLearping;
+
+    private Vector3 _startPosition, _endPosition;
+    private float _timeStartedLearping;
+
+    //stepsize
+    private int _number;
+    private int _currentIteration;
+    private TrailRenderer _trailRenderer;
+
+    private Vector2 CalculatePhyllotaxis(float degree, float scale, int number)
+    {
+        double angle = number * (degree * Mathf.Deg2Rad);
+
+        float r = scale * Mathf.Sqrt(number);
+
+        //declaring x and y radius
+        float x = r * (float)System.Math.Cos(angle);
+        float y = r * (float)System.Math.Sin(angle);
+
+        Vector2 vec2 = new Vector2(x, y);
+
+        return vec2;
+    }
+
+    private Vector2 _phyllotaxisPosition;
+
+    void StartLearping()
+    {
+        _isLearping = true;
+        _timeStartedLearping = Time.time;
+        _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _scale, _number);
+        _startPosition = this.transform.localPosition;
+        _endPosition = new Vector3(_phyllotaxisPosition.x, _phyllotaxisPosition.y, 0);
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        _trailRenderer = GetComponent<TrailRenderer>();
+        _number = _numberStart;
+        transform.localPosition = CalculatePhyllotaxis(_degree, _scale, _number);
+        if (_useLearping)
+        {
+            StartLearping();
+        }
+
+    }
+
+    //
+    private void FixedUpdate()
+    {
+        if (_useLearping)
+        {
+            if (_isLearping)
+            {
+                float timeSinceStarted = Time.time - _timeStartedLearping;
+                float percentageComplete = timeSinceStarted / _intervalLerp;
+                transform.localPosition = Vector3.Lerp(_startPosition, _endPosition, percentageComplete);
+
+                if (percentageComplete >= 0.97f)
+                {
+                    transform.localPosition = _endPosition;
+                    _number += _stepSize;
+                    _currentIteration++;
+                    if (_currentIteration <= _maxIntegration)
+                    {
+                        StartLearping();
+                    }
+                    else
+                    {
+                        _isLearping = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            _phyllotaxisPosition = CalculatePhyllotaxis(_degree, _scale, _number);
+            transform.localPosition = new Vector3(_phyllotaxisPosition.x, _phyllotaxisPosition.y, 0);
+            _number += _stepSize;
+            _currentIteration++;
+        }
+    }
+}
